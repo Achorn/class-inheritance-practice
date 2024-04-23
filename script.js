74a6a58edb0e795894c2dc2618af6997229c9dd6
achorn <joshuaachorn@gmail.com>
2024-04-23 12:31:13 -0700

    updated document load to window load. added delta time for animation frame

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-document.addEventListener("load", () => {
+console.log("hello?");
+
+//document 'load' unreliable
+window.addEventListener("load", (e) => {
   // waits for all resources to load before running any code
 
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
-
   canvas.height = 800;
+  let lastTime = 0;
 
   // wrapper containing and controlling all movement and animation logic
   class Game {
@@ -25,10 +28,14 @@ document.addEventListener("load", () => {
     draw() {}
   }
 
-  function animate() {
+  function animate(timestamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+    const deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+    console.log(deltaTime);
     //some code
     requestAnimationFrame(animate);
   }
+
+  animate(0);
 });
