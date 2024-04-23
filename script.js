2fb6bc584a8c4210403a74baa401d89e5deab841
achorn <joshuaachorn@gmail.com>
2024-04-23 12:42:53 -0700

    passing game class to children so they have access to height width etc.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("hello?");
-
 //document 'load' unreliable
 window.addEventListener("load", (e) => {
   // waits for all resources to load before running any code
@@ -8,34 +6,57 @@ window.addEventListener("load", (e) => {
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
   canvas.height = 800;
-  let lastTime = 0;
 
   // wrapper containing and controlling all movement and animation logic
   class Game {
-    constructor() {
+    constructor(ctx, width, height) {
+      this.ctx = ctx;
+      this.width = width;
+      this.height = height;
       this.enemies = [];
+      this.#addNewEnemy();
+      console.log(this.enemies);
     }
-    update() {}
-    draw() {}
+    update() {
+      this.enemies.forEach((enemy) => enemy.update());
+    }
+    draw() {
+      this.enemies.forEach((enemy) => enemy.draw());
+    }
 
     //# means private
-    #addNewEnemy() {}
+    #addNewEnemy() {
+      this.enemies.push(new Enemy(this));
+    }
   }
 
   class Enemy {
-    constructor() {}
-    update() {}
-    draw() {}
+    constructor(game) {
+      this.game = game;
+      console.log(game);
+      this.x = this.game.width;
+      this.y = Math.random() * this.game.height;
+      this.width = 100;
+      this.height = 100;
+    }
+    update() {
+      this.x--;
+    }
+    draw() {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
 
+  const game = new Game(ctx, canvas.width, canvas.height);
+  let lastTime = 1;
   function animate(timestamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     const deltaTime = timestamp - lastTime;
     lastTime = timestamp;
-    console.log(deltaTime);
     //some code
+    game.update();
+    game.draw();
     requestAnimationFrame(animate);
   }
-
   animate(0);
 });
